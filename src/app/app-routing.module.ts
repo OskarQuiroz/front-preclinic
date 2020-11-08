e734e03c33179ba0a31bf689260496af48d7bd2e
jairo <jairo.obregon@unmsm.edu.pe>
2020-11-08 17:57:38 -0500

    base-proyecto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,70 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { EstadisticaComponent } from './estadistica/estadistica.component';
+import { FichaMedicaComponent } from './ficha-medica/ficha-medica.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+import { NavVerticalComponent } from './nav-vertical/nav-vertical.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RolesComponent } from './roles/roles.component';
+import { CrearUsuarioComponent } from './usuarios/crear-usuario/crear-usuario.component';
+import { PerfilUsuarioComponent } from './usuarios/perfil-usuario/perfil-usuario.component';
+import { UsuariosComponent } from './usuarios/lista-usuario/usuarios.component';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    component: NavVerticalComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/inicio',
+        pathMatch: 'full',  
+      },
+      {
+        path: 'inicio',
+        component: InicioComponent
+      },
+      {
+        path: 'usuarios',
+        component: UsuariosComponent
+      },
+        {
+          path: 'crear-usuario',
+          component: CrearUsuarioComponent
+        },
+        {
+          path: 'perfil-usuario', //solo es de muestra, en realiddad el path tiene que ser usuarios/:id donde el id es el idetificador del usuario
+          component: PerfilUsuarioComponent
+        },
+        {
+          path: 'perfil-usuario/ficha-medica', 
+          component: FichaMedicaComponent
+        },
+      {
+        path: 'estadistica',
+        component: EstadisticaComponent
+      },
+      {
+        path: 'roles',
+        component: RolesComponent
+      },
+      
+    ]
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  },
+ 
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavVerticalComponent } from './nav-vertical/nav-vertical.component';
+import { LoginComponent } from './login/login.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { UsuariosComponent } from './usuarios/lista-usuario/usuarios.component';
+import { EstadisticaComponent } from './estadistica/estadistica.component';
+import { FichaMedicaComponent } from './ficha-medica/ficha-medica.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RolesComponent } from './roles/roles.component';
+import { CrearUsuarioComponent } from './usuarios/crear-usuario/crear-usuario.component';
+import { PerfilUsuarioComponent } from './usuarios/perfil-usuario/perfil-usuario.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavVerticalComponent,
+    LoginComponent,
+    InicioComponent,
+    UsuariosComponent,
+    EstadisticaComponent,
+    FichaMedicaComponent,
+    NotFoundComponent,
+    RolesComponent,
+    CrearUsuarioComponent,
+    PerfilUsuarioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
