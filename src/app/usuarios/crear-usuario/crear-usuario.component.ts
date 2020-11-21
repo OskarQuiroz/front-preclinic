6dd04b77342d7b3b5c66fde46f7ba6352f660214
Evison <evisoncy@gmail.com>
2020-11-20 21:42:17 -0500

    crear usuario

--- src/app/usuarios/crear-usuario/crear-usuario.component.ts
+++ src/app/usuarios/crear-usuario/crear-usuario.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import { CrearUsuarioService } from './service/crear-usuario.service';
 
 @Component({
   selector: 'app-crear-usuario',
   templateUrl: './crear-usuario.component.html',
-  styleUrls: ['./crear-usuario.component.css']
+  styleUrls: ['./crear-usuario.component.css'],
+  providers: [CrearUsuarioService]
 })
 export class CrearUsuarioComponent implements OnInit {
 
-  constructor() { }
+  titulo = "Crear Usuario";
+  //form: FormGroup;
+  codigo: String;
+  nombres: String;
+  apellidos: String;
+  dni:String;
+  email:String;
+  password: String;
+  direccion:String;
+  telefono:String;
+  
+
+  constructor(private serviceUser: CrearUsuarioService ) { }
 
   ngOnInit(): void {
+
   }
 
+  user(){
+    console.log(this.codigo)
+    console.log(this.nombres)
+    console.log(this.apellidos) 
+    console.log(this.dni) 
+    console.log(this.email) 
+    console.log(this.password) 
+    console.log(this.direccion) 
+    console.log(this.telefono) 
+     
+    
+    this.serviceUser.User(this.codigo, this.nombres, this.apellidos, this.dni, this.email, this.password, this.direccion, this.telefono).subscribe(   
+      (data) => {
+        console.log(data)
+        
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  }
+
+ /*guardar(){
+    this.serviceUser.User(this.codigo, this.nombres, this.apellidos, this.dni, this.email, this.password, this.direccion, this.telefono, )
+      .subscribe(  
+        rt => console.log(rt),
+        er => console.log(er),
+        () => console.log('fin')
+      );
+
+  }*/
 }
--- src/app/usuarios/crear-usuario/service/crear-usuario.service.ts
+++ src/app/usuarios/crear-usuario/service/crear-usuario.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrearUsuarioService {
+  httpOptions: any;
 
-  constructor() { }
+  constructor(private httpCliente: HttpClient) {
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+      })
+    };
+   }
 
+  User(codigo: String, nombres: String, apellidos:String, dni:String, email:String, password:String, direccion:String, telefono:String){
+    
+    return this.httpCliente.post('https://nameless-plains-49486.herokuapp.com/api/users', {"codigo":codigo, "nombres":nombres, "apellidos":apellidos, "dni":dni, "email": email, "password":password, "direccion":direccion, "telefono":telefono },this.httpOptions);
+
+  }
+}
